19015ff5ab487d8890bc6a7c6779a08a8196e111
Lochan Chhetri <lochan.chhetri@gmail.com>
2020-02-28 16:15:23 -0800

    Rest of the CRUD operations

--- routes/operators.js
+++ routes/operators.js
@@ -3,8 +3,16 @@ const Operator = require('../models/Operator');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('We are on operators');
+router.get('/', async (req, res) => {
+    Operator.find()
+        .then( data => res.json(data) )
+        .catch(err => res.json({ message: err }));
+});
+
+router.get('/:id', (req, res) => {
+    Operator.findById(req.params.id)
+        .then(data => res.json(data))
+        .catch(err => res.json({ message: err }));
 });
 
 router.post('/', (req, res) => {
@@ -13,10 +21,22 @@ router.post('/', (req, res) => {
         ShortName: req.body.ShortName
     });
 
-    operator.save().then( data => {
-        res.json(data)
-    }).catch( err => res.json({message: err}) );
+    operator.save()
+    .then( data => res.json(data))
+    .catch( err => res.json({message: err}) );
 
+});
+
+router.patch('/:id', (req, res) => {
+    Operator.updateOne({_id: req.params.id}, { $set: { ShortName: req.body.ShortName } })
+        .then(data => res.json(data))
+        .catch(err => res.json({ message: err }));
+});
+
+router.delete('/:id', (req, res) => {
+    Operator.remove({ _id: req.params.id })
+        .then( data => res.json(data))
+        .catch( err => res.json({message: err}));
 });
 
 module.exports = router;
